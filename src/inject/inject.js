dbddf1ab20bcca91ec0a039f3d28ca1ab1ee2306
Nick <nicholas.o.lockhart@gmail.com>
2013-08-02 11:39:40 +1200

    More specific selector for the account number input

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,1 +1,5 @@
-$('table').paster();
+//The table containing the payee input fields
+var payeeTable = $("#payeeAccount_radNewPayeeAccount").closest('table');
+
+//Init the paster plugin
+$(payeeTable).paster();
