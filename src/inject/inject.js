2e8fc8ac829ae59b7b05582f33101c48a4599eaf
Nick <nicholas.o.lockhart@gmail.com>
2013-08-07 17:06:26 +1200

    Added qTip2, bit of UX love

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,3 +3,16 @@ var payeeTable = $("#payeeAccount_radNewPayeeAccount").closest('table');
 
 //Init the paster plugin
 $(payeeTable).paster();
+
+$('#payeeAccount_accountNumber_txtBank').qtip({ // Grab some elements to apply the tooltip to
+    content: {
+        text: 'You can paste full bank account numbers here.'
+    },
+    position: {
+            at: 'top center',
+            my: 'bottom center'
+    },
+    style: {
+    	classes: 'qtip-light'
+    }
+})
